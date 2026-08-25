d20f539eb4e1c779b3dc05eeb5f3966762527a66
Bruno Darlei Dos Santos <brunodarlei2004@gmail.com>
2026-08-25 10:25:39 -0300

    FEAT: resolução exercício 4

--- Ex4/CodigoEx4.c
+++ Ex4/CodigoEx4.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    
+    int y = 5;
+    int z = 11;
+    int w;
+    w = y + z; // w = 11+5 = 16
+
+    if (y > z) { // 5 > 11 = falso, então não entramos no if
+        w = y * z; 
+    }
+
+    printf("%d", w); // w = 16.
+    return 0;
+
+    // conclusão: a saída será 16, pois a condição do if não é satisfeita, então o valor de w não é alterado.
 }
